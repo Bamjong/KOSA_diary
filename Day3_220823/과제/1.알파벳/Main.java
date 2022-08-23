e4118701806f77ee7925e713d745729e549c6b43
Bamjong <qjawhd100@naver.com>
2022-08-23 17:31:45 +0900

    과제 추가

---
+++ Day3_220823/과제/1.알파벳/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        for (int i = 65; i <= 90; i++) {
+            int Alphabet = i;
+            System.out.print((char)Alphabet + " ");
+        }
+    }
+}

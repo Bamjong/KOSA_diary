bde6f719b2ad771e8de570b50f372e0b5de0d928
Bamjong <qjawhd100@naver.com>
2022-08-26 10:48:13 +0900

    커밋

---
+++ Ex05_Modifier.java
@@ -0,0 +1,24 @@
+import Day6_220826.과제.kr.or.kosa.Car;
+
+
+public class Ex05_Modifier {
+	public static void main(String[] args) {
+		Car car = new Car ();
+		
+		int speed = car.getSpeed();
+		System.out.println("speed: " + speed);
+		
+		car.speedUp();
+		car.speedUp();
+		
+		speed = car.getSpeed();
+		System.out.println("speed: " + speed);
+		
+		car.speedDown();
+		car.speedDown();
+		car.speedDown();
+		
+		speed = car.getSpeed();
+		System.out.println("speed: " + speed);
+	}
+}

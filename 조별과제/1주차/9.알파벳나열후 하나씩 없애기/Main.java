e6d15e5aaba0550a77522cb36ab2575f126fdcad
Bamjong <qjawhd100@naver.com>
2022-08-29 01:18:09 +0900

    커밋!

--- 조별과제/1주차/9.알파벳나열후 하나씩 없애기/Main.java
+++ 조별과제/1주차/9.알파벳나열후 하나씩 없애기/Main.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int alpha = Integer.parseInt(sc.nextLine());
+        
+
+        System.out.println(alpha);
+
+        
+    }
 }
